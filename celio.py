df2eb3f997b367d5fedf5484321ec90f068da261
matheusmdsm <mmagalhaes67@gmail.com>
2023-09-06 18:11:58 -0300

    beta version

--- celio.py
+++ celio.py
@@ -1,30 +1,28 @@
 import logging
-from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, Updater
-import requests
-from msgs import *
-from reset_senha import *
-
+from telegram import *
+from telegram.ext import *
+from reset_senha import ResetXiongmaiDate
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-
-TOKEN = '6538656591:AAG5fMTY0teI_wEVi6t2LeHeXIqH4XR6yIw'
+TOKEN = '6538656591:AAG5fMTY0teI_wEVi6t2LeHeXIqH4XR6yIw'  # Substitua pelo seu token
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user 
+    custom_keyboard = [[InlineKeyboardButton("Reset de Senha", callback_data='button_reset_clicked')]]
+    reply_markup = InlineKeyboardMarkup(custom_keyboard)
+    saudacao = f'Olá, {user.first_name}👋!\nMeu nome é Célio, sou o chatbot da Clear CFTV!\nPosso te ajudar em algumas coisas.'
     
-    
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=saudacao)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=opcoes)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=saudacao, reply_markup=reply_markup)
 
-async def reset(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def reset_button_callback(update, context):
+    query = update.callback_query
+    query.answer()
     
-    
-    senha = ResetXiongmaiDate(20230905)
-    
+    senha = ResetXiongmaiDate(20230905)  # Certifique-se de que ResetXiongmaiDate esteja definida corretamente
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Atualmente, eu ainda só consigo fazer o reset de senha dos DVRs da linha Xmeye")
     await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Aqui está 🫡!\n{senha}")
 
@@ -33,13 +31,8 @@ if __name__ == '__main__':
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
-    
-    reset_handler = CommandHandler('reset', reset)
-    application.add_handler(reset_handler)
+
+    reset_button_handler = CallbackQueryHandler(reset_button_callback, pattern='^button_reset_clicked$')
+    application.add_handler(reset_button_handler)
 
     application.run_polling()
-
-
-    
-
-    
--- msgs.py
+++
@@ -1,3 +0,0 @@
-saudacao = 'Olá 👋!\nMeu nome é Célio, sou o chatbot da Clear CFTV!\nPosso te ajudar em algumas coisas.'
-
-opcoes = 'Atendimento Comercial - Clique em 👉 /comercial\nSuporte Técnico - Clique em 👉 /suporte'
