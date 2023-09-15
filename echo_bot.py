c9c2b7de14ebfb8a2836fca94fe1f26717cd0b37
matheusmdsm <mmagalhaes67@gmail.com>
2023-09-15 11:30:18 -0300

    versao atualizada

--- echo_bot.py
+++ echo_bot.py
@@ -7,29 +7,34 @@ from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 with open('config.txt', "r") as config_file:
     TOKEN = config_file.read().strip()
 
+# Informações de log
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)  # Outputs debug messages to console.
 
 bot = telebot.TeleBot(TOKEN)
 
 # Dicionário para armazenar o estado do usuário
-user_state = {}
-conversation_state = {}
+user_state = {} # Rastreia se o usuário já enviou a data ou código key no processo de reset de senha
+conversation_state = {} # Utilizado para rastrear se o usuário já inciou um atendimento ou não, se não iniciou, o message handler com message:True entra em ação
 
+# Start Point 
 @bot.message_handler(commands=['start', 'voltar'])
 def start_message(message):
     chat_id = message.chat.id
     user_firstname = message.from_user.first_name
+    conversation_state[chat_id]='em andamento'
     msg = f"Olá, {user_firstname} 👋!\nMeu nome é Célio, sou o chatbot da Clear CFTV!\nPosso te ajudar em algumas coisas:"
     markup = InlineKeyboardMarkup()
     markup.row_width = 2
-    custom_keyboard = [InlineKeyboardButton('Veicular', callback_data='callback_veicular'),
-                       InlineKeyboardButton('CFTV', callback_data='callback_cftv')]
+    custom_keyboard = [InlineKeyboardButton('Atendimento Comercial', callback_data='callback_comercial'),
+                       InlineKeyboardButton('Suporte Técnico', callback_data='callback_suporte')]
     
     markup.add(custom_keyboard[0], custom_keyboard[1])
 
     bot.send_message(message.chat.id, msg, reply_markup=markup)
 
+# Message Handlers - Respostas aos comandos
+# Message Handlers - Suporte Técnico
 @bot.message_handler(func=lambda message: user_state.get(message.chat.id) == 'esperando_key')
 def handle_key(message):
     chat_id = message.chat.id
@@ -55,7 +60,6 @@ def sair(message):
         del user_state[chat_id]
     bot.send_message(chat_id, f'Espero ter te ajudado! Até breve, {user_firstname} 👋')
 
-
 @bot.message_handler(commands=['ajuda'])
 def ajuda(message):
     chat_id = message.chat.id
@@ -66,19 +70,54 @@ def ajuda(message):
     with open('media\cftv\imgs\key.png', 'rb') as photo:
         bot.send_photo(chat_id, photo,'Se seu DVR tiver este símbolo -> ❓  então selecione a opção [Código Key]. Caso não tenha, selecione a opção [Reset por Data]', reply_markup=reply)
 
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    chat_id = message.chat.id
-    if conversation_state.get(chat_id) is None:
-        bot.reply_to(message, 'Digite /start para começar')
-        conversation_state[chat_id] = "em_andamento"
-    elif conversation_state.get(chat_id) == "em_andamento":
-        # Não faça nada quando a conversa está em andamento
-        pass
-    else:
-        # Lidere com mensagens quando a conversa não está em andamento
-        pass
+# Message Handler Comercial
+@bot.message_handler(commands=['veicular'])
+def veicular(message):
+    bot.send_message(message.chat.id, 'Aqui vai um [vídeo](https://www.youtube.com/watch?v=SqESxWL17bQ) para você conhecer mais sobre nossa linha veicular: ', parse_mode='Markdown')
+    bot.send_message(message.chat.id, 'Para receber nosso Catálogo, me envie\n/catalogoveicular, ou clique no comando que eu envio para você... ')
+    bot.send_message(message.chat.id, 'Se desejar encerrar seu atendimento, digite /sair ou se quiser retornar ao início, digite /voltar')
 
+@bot.message_handler(commands=['catalogoveicular'])
+def catalogoveicular(message):    
+    bot.send_message(message.chat.id, 'Ok! Um momento... ')
+    with open('media/veicular/docs/CatalogoVeicular.pdf', 'rb') as catalogo_veicular:
+        bot.send_document(message.chat.id,catalogo_veicular, caption='Aqui está!')
+    bot.send_message(message.chat.id, 'Se desejar encerrar seu atendimento, digite /sair ou se quiser retornar ao início, digite /voltar')
+
+
+@bot.message_handler(commands=['cftv','CFTV'])
+def cftv(message):
+    bot.send_message(message.chat.id, 'Vou te encaminhar nosso catálogo de produtos para você conhecer nossas novidades...')
+    with open('media\cftv\docs\catalogo_cftv.pdf','rb') as catalogo_cftv:
+        bot.send_document(message.chat.id, catalogo_cftv, caption='Aqui está! Se tiver dúvidas, entre em contato com nossos consultores, será um prazer te ajudar...')
+    bot.send_message(message.chat.id, 'Se desejar encerrar seu atendimento, digite /sair ou se quiser retornar ao início, digite /voltar')
+ 
+# Callback Query Handlers - Respostas aos botões
+# Callback Comercial
+@bot.callback_query_handler(func=lambda call: call.data == 'callback_comercial')
+def callback_comercial(call):
+    user_firstname = call.from_user.first_name
+    msg = f'{user_firstname}, aqui vai algumas opções que posso fazer por você:\n\n\
+☎️ - Fale conosco: 3534734000\n\
+🚛 - Conheça nossa linha /veicular\n\
+🎥 - Conheça nossa linha /CFTV\n\
+💻 - Acesse nosso [site](www.clearcftv.com.br)👇\n\n\
+                             '
+    bot.send_message(call.message.chat.id, msg, parse_mode='Markdown')
+
+# Callback Suporte Técnico 👇👇
+@bot.callback_query_handler(func=lambda call: call.data == 'callback_suporte')
+def callback_suporte(call):
+    conversation_state[call.message.chat.id]='em andamento'
+    msg = "Vejo que você precisa de ajuda com nossos produtos.\nSelecione a vertical de produtos que precisa de suporte. 👇"
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    custom_keyboard = [InlineKeyboardButton('Veicular', callback_data='callback_veicular'),
+                       InlineKeyboardButton('CFTV', callback_data='callback_cftv')]
+    
+    markup.add(custom_keyboard[0], custom_keyboard[1])
+
+    bot.send_message(call.message.chat.id, msg, reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: call.data == 'callback_veicular')
 def callback_veicular(call):
@@ -131,7 +170,7 @@ def handle_data(message):
     except ValueError:
         bot.send_message(chat_id, 'Não foi possível entender o que você escreveu. Tente novamente clicando no botão "Reset de Senha" e verifique se digitou corretamente.')
     
-    user_state.pop(chat_id)  # Remova o estado do usuário após a conclusão
+    del user_state[chat_id]  # Remova o estado do usuário após a conclusão
 
 @bot.callback_query_handler(func=lambda call: call.data == 'callback_reset_key')
 def callback_reset_key(call):
@@ -144,10 +183,19 @@ def callback_reset_key(call):
         bot.send_message(chat_id, 'Envie o código Key que está sendo exibido na tela e aguarde alguns instantes...')
         user_state[chat_id] = 'esperando_key'
 
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    chat_id = message.chat.id
+    if conversation_state.get(chat_id) is None:
+        bot.reply_to(message, 'Digite /start para começar')
+        conversation_state[chat_id] = "em_andamento"
+    elif conversation_state.get(chat_id) == "em_andamento":
+        # Não faça nada quando a conversa está em andamento
+        pass
+    else:
+        # Lidere com mensagens quando a conversa não está em andamento
+        pass
 
-
-
-
-bot.polling()
+bot.infinity_polling()
 
 
--- reset_senha.py
+++ reset_senha.py
@@ -2,45 +2,44 @@ from bs4 import BeautifulSoup
 import requests, time
 
 def ResetXiongmaiDate(cod,tipo):
-        cont = 0
-        url = 'https://cctvwiki.com/en/xiongmai-tech-password-reset/'
+    senha = 'Super Password: Wait 5s and try it again'
+    while senha == 'Super Password: Wait 5s and try it again': # Loop para caso o servidor não responda com a senha, ele tente por até 3 vezes... 
+        cont = 0 # Tentativas de conseguir a senha
+        url = 'https://cctvwiki.com/en/xiongmai-tech-password-reset/' # URL da API
+        data = {'txtCode': cod} # Parâmetro que eu vou passar para fazer o "POST"
+        response = requests.post(url, data=data) # Faço o POST na URL
+        
+        if response.status_code == 200: # Se o servidor responder
+            html = response.text # Crio um html de resposta
 
-        data = {'txtCode': cod}
+            # Cria um objeto BeautifulSoup para analisar o HTML
+            soup = BeautifulSoup(html, 'html.parser')
 
-        response = requests.post(url, data=data)
+            # Encontra o elemento <span> com id="KQ"
+            span_element = soup.findAll('span', {'id': 'KQ'})
 
-        if response.status_code == 200:
-                html = response.text
-                # Cria um objeto BeautifulSoup para analisar o HTML
-                soup = BeautifulSoup(html, 'html.parser')
-
-                # Encontra o elemento <span> com id="KQ"
-                span_element = soup.findAll('span', {'id': 'KQ'})
-
-                # Verifica se o elemento foi encontrado
-                if span_element:
-                    # Obtém o texto dentro do elemento <span>
-                    if tipo == 'date':
-                        senha = span_element[0].get_text()
-                        parts = senha.split("Time:")
-                        senha = parts[0]
-                    elif tipo == 'key':
-                        senha = span_element[1].get_text()
-                        parts = senha.split("Time:")
-                        senha = parts[0]
-                    
-                    if senha == 'Super Password: Wait 5s and try it again':
-                         time.sleep(6)
-                         ResetXiongmaiDate(cod,tipo)
-                         cont+=1
-                         if cont > 3:
-                            senha = 'Erro! tente novamente em alguns minutos...'
-                            return senha
-                    else:
-                        return senha
-                else:
-                    senha = "Erro! tente novamente em alguns minutos..."
-                    return senha
+            # Verifica se o elemento foi encontrado
+            if span_element:
+                # Obtém o texto dentro do elemento <span>
+                if tipo == 'date':
+                    senha = span_element[0].get_text()
+                    parts = senha.split("Time:")
+                    senha = parts[0]
+                    if senha != 'Super Password: Wait 5s and try it again':
+                        break
+                elif tipo == 'key':
+                    senha = span_element[1].get_text()
+                    parts = senha.split("Time:")
+                    senha = parts[0]
+                    if senha != 'Super Password: Wait 5s and try it again':
+                        break
+            cont += 1
+            time.sleep(5)
+            if cont > 2:
+                senha = 'Lamento, mas o servidor de reset de senha está indisponível no momento... Aguarde alguns minutos e tente novamente...'
+                break
         else:
-            senha = 'Erro! tente novamente em alguns minutos...'
-            return senha 
+            senha = "Lamento, mas o servidor de reset de senha está indisponível no momento... Aguarde alguns minutos e tente novamente...",
+            break
+            
+    return senha
